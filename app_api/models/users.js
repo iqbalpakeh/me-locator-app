6a3396768966ac16655cf2d58c3ceb01951bc933
Progrema Studio <progrema.studio@gmail.com>
2018-10-24 19:26:26 +0800

    BUG#1 : comment out hash calculation function

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,4 +1,4 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 
@@ -16,17 +16,25 @@ var userSchema = new mongoose.Schema({
   salt: String
 });
 
-userSchema.methods.setPassword = function(password){
+userSchema.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  /**
+   * BUG#1
+   * This function didn't return and make the app hang. 
+   * Find out why it doesn't work on this machine!!
+   */
+  this.hash = ""; //crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
-userSchema.methods.validPassword = function(password) {
+userSchema.methods.validPassword = function (password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-  return this.hash === hash;
+  /**
+   * Always return because hash can't be calculated due to the BUG#1
+   */
+  return true //this.hash === hash;
 };
 
-userSchema.methods.generateJwt = function() {
+userSchema.methods.generateJwt = function () {
   var expiry = new Date();
   expiry.setDate(expiry.getDate() + 7);
 
